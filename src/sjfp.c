3946db34c49d1f6d9a0c2f7afa93de4f14ad0857
Muneeb Shaikh <iammuneeb@gmail.com>
2011-02-09 17:33:06 +0530

    selectProcess function finished with 1 limitation
    limitation: the function is unable to return the correct index value if two
    burstTime values are same
    i.e. if burstTime[i] == burstTime[j] where i<j in this case it should
    return i but it returns j. The probable solution would be to keep track
    of equal burst time values and then return index.

--- src/sjfp.c
+++ src/sjfp.c
@@ -11,11 +11,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define TRUE 1
-#define FALSE 0
-
-static int currentTime=1;
+static int currentTime=0;
 int numOfProcesses;
+int currentRunningProcess;
 
 typedef struct {
 	char name[3];
@@ -23,10 +21,9 @@ typedef struct {
 	int burstTime;
 	int startTime;
 	int finishTime;
-	int waitngTime;
-	int turnaroundTime;
-	int IsActive;
-	int IsArrived;
+	int waitingTime;
+//	int turnaroundTime;
+	int IsActiveFor;
 }proc;
 
 void psort(proc *, int);			/* Sorts Processes according to Burst Time */
@@ -36,7 +33,7 @@ int getTotalBurstTime(proc *, int); /* Calculates total burst Time */
 
 int main(void) {
 	proc process[10];
-	int i;
+	int i, totolServiceTime;
 
 	puts("Shortest Job First Pre-emptive Algorithm\n");
 	puts("Enter Number of Process: ");
@@ -52,8 +49,8 @@ int main(void) {
 
 		// Initialise other properties accordingly
 		process[i].startTime = 0;
-		process[i].waitngTime = 0;
-		process[i].IsActive = process[i].IsArrived = FALSE;
+		process[i].waitingTime = 0;
+		process[i].IsActiveFor = 0;
 
 	}
 
@@ -65,21 +62,20 @@ int main(void) {
 
 	psort(process, numOfProcesses);
 //	puts("After Sorting \n");
-
+	totolServiceTime = getTotalBurstTime(process, numOfProcesses);
 	execProcess(process, getTotalBurstTime(process, numOfProcesses ));
 
-	printf("PName\tArrTime\tBTime\tSTime\tFTime\tWTime\tTATime\n");
+	printf("PName\tArrTime\tBTime\tSTime\tFTime\tWTime\n");
 
 	for(i=0; i<numOfProcesses; i++) {
-		printf("\n%s\t%d\t%d\t%d\t%d", process[i].name, process[i].arrvialTime, process[i].burstTime,
-				process[i].startTime, process[i].finishTime);
+		printf("\n%s\t%d\t%d\t%d\t%d\t%d", process[i].name, process[i].arrvialTime, process[i].burstTime,
+				process[i].startTime, process[i].finishTime, process[i].waitingTime);
 	}
 
-	printf("\nTotal Burst Time: %d \n", getTotalBurstTime(process, numOfProcesses) );
+	printf("\nTotal Burst Time: %d \n", totolServiceTime );
 
-//	execProcess(process, getTotalBurstTime(process, numOfProcesses ));
 
-	printf("All Processes executed...");
+	printf("All Processes executed...\n");
 
 	return EXIT_SUCCESS;
 }
@@ -104,18 +100,21 @@ void psort(proc pproc[], int numOfProcesses) {
 }
 
 int selectProcess(proc pproc[]) {
+
 	int i=0, j, indexOfleastProcessTime, minBurstTime;
 	indexOfleastProcessTime = -1;
 
-	while ( pproc[i].arrvialTime <= currentTime && i < numOfProcesses ) {
-		i++; 		/* Find array of arrived processes */
+	while ( pproc[i].arrvialTime <= currentTime && i < numOfProcesses) {
+		i++; 									/* Find array of arrived processes*/
 	}
+
+	i--;
 
 	for(j=0; j <= i && pproc[j].burstTime == 0; j++); /* Find first non-zero value in array*/
 
 	minBurstTime = pproc[j].burstTime;
 
-	while(j<i) {
+	while(j<=i) {
 		if(pproc[j].burstTime != 0 && pproc[j].burstTime <= minBurstTime) {
 			minBurstTime = pproc[j].burstTime;
 			indexOfleastProcessTime = j;
@@ -126,30 +125,40 @@ int selectProcess(proc pproc[]) {
 	if(indexOfleastProcessTime == 0 && pproc[indexOfleastProcessTime].startTime == 0) {
 		pproc[indexOfleastProcessTime].startTime = 0;
 	} else if(pproc[indexOfleastProcessTime].startTime == 0) {
-		pproc[indexOfleastProcessTime].startTime = currentTime-1;
+		pproc[indexOfleastProcessTime].startTime = currentTime;
 	}
+
+	currentRunningProcess = indexOfleastProcessTime;
 
 	return indexOfleastProcessTime;
 }
 
 
-
 void execProcess(proc pproc[], int totalBurstTime) {
 	int indexOfleastProcessTime;
 
-	while (currentTime != totalBurstTime+1) { /*Terminating Condition*/
+	while (currentTime != totalBurstTime) { /*Terminating Condition*/
 		printf("\n\nFetching next Shortest Process...\n");
 
 		indexOfleastProcessTime = selectProcess(pproc); /* Call to process selection function */
 
 		printf("\nExecuting Process %s \n", pproc[indexOfleastProcessTime].name);
 		pproc[indexOfleastProcessTime].burstTime -= 1;
+		pproc[indexOfleastProcessTime].IsActiveFor += 1;
 
+		if(pproc[indexOfleastProcessTime].burstTime == 0){
 
-		if(pproc[indexOfleastProcessTime].burstTime == 0)
-			pproc[indexOfleastProcessTime].finishTime = currentTime;
+			pproc[indexOfleastProcessTime].finishTime = currentTime+1;
+/*
+ * 		This waiting time is total waiting time. It means that idle time spent by the process since it has arrived
+ * 		pproc[indexOfleastProcessTime].waitingTime = pproc[indexOfleastProcessTime].finishTime-pproc[indexOfleastProcessTime].IsActiveFor-pproc[indexOfleastProcessTime].startTime;
+ *
+ */
 
-		system("sleep 1");
+			pproc[indexOfleastProcessTime].waitingTime = pproc[indexOfleastProcessTime].startTime - pproc[indexOfleastProcessTime].arrvialTime ;
+		}
+
+//		system("sleep 1");
 		currentTime += 1;
 	}
 
@@ -162,8 +171,8 @@ int getTotalBurstTime(proc pproc[], int numOfProcesses) {
 		sum += pproc[i].burstTime;
 	}
 	return sum;
-}
 
+}
 
 
 
